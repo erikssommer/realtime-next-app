13ff9486af5dec28d7a9ca34d5289931549800d6
Erik Storås Sommer <erikssommer@gmail.com>
2023-05-03 20:26:53 +0200

    Setting up button

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import { FC } from 'react';
+import Button from '@/components/ui/Button';
+
+interface pageProps { }
+
+const page: FC<pageProps> = ({ }) => {
+    return <div><Button>Hello</Button></div>;
+};
+
+export default page;
